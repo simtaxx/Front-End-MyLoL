ef136eb82d71a3a135c966ee76c6c8787ad3f889
Haris Souici <haris@MacBook-Pro-de-Haris.local>
2020-08-24 23:17:58 +0200

    feature: first commit add all dependcies needed / add typescript

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="typescript">
+  import { timer } from './timer';
+
+  let a: number = 42;
+</script>
+
+<main>
+  <h2>Count is {$timer}</h2>
+  <p>What's the meaning of life? {a}</p>
+</main>
